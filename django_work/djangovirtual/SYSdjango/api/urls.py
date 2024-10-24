ddf4106c95e1b0433e1f464ce26fed28ef6b51ef
wade-ary <161520052+wade-ary@users.noreply.github.com>
2024-10-23 20:40:38 -0400

    commit 1 to api

--- django_work/djangovirtual/SYSdjango/api/urls.py
+++ django_work/djangovirtual/SYSdjango/api/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('product-update/<int:pk>/', views.UpdateProduct, name='product-update'),
     path('product-delete/<int:pk>/', views.DeleteProduct, name='product-delete'),
 
-    path('product-list/', views.ShowAll, name='product-list'),
-    path('product-detail/<int:pk>/', views.ViewProduct, name='product-detail'),
-    path('product-create/', views.CreateProduct, name='product-create'),
-    path('product-update/<int:pk>/', views.UpdateProduct, name='product-update'),
-    path('product-delete/<int:pk>/', views.DeleteProduct, name='product-delete'),
+    path('user-list/', views.ShowAllUsers, name='user-list'),
+    path('user-detail/<int:pk>/', views.ViewUser, name='user-detail'),
+    path('user-create/', views.CreateUser, name='user-create'),
+    path('user-update/<int:pk>/', views.UpdateUser, name='user-update'),
+    path('user-delete/<int:pk>/', views.DeleteUser, name='user-delete'),
 
 ]
 
