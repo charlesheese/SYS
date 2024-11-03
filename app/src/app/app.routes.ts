1d5ee6fb9a210a4457404f076a66785040fcfd5b
johnsantosa <santosa.j@northeastern.edu>
2024-11-03 17:04:56 -0500

    login and signup page 1

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   {'path': 'home', 'title': 'Home', component: HomeComponent, 'data':{'breadcrumb':'Home'}},
   {'path': '',   redirectTo: '/home', pathMatch: 'full' },
-  {'path' : 'login', 'title' : 'Login page', component: LoginComponent},
+  {'path' : 'login', 'title' : 'Log In page', component: LoginComponent},
+  {'path' : 'signup', 'title' : 'Sign Up page', component: SignupComponent},
   
 ];
